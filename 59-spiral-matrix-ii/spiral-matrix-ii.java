d3dabb3a0dc4c3dd995da1449cf17383b2f5ceb4
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2024-10-21 14:08:35 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (18.57%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -1,33 +1,30 @@
 class Solution {
     public int[][] generateMatrix(int n) {
-         int[][] arr=new int[n][n];
+        int[][] arr=new int[n][n];
         int t=0;
-        int b=n-1;;
         int l=0;
-        int r=n-1;
+        int b=n-1, r=n-1;
         int c=1;
-        while(t<=b && l<=r){
-        for(int i=l;i<=r;i++){
-           arr[t][i]=c++;
-        }
-        t++;
-        for(int i=t;i<=b;i++){
-            arr[i][r]=c++;
-        }
-        r--;
-       if(t<=b){
-        for(int i=r;i>=l;i--){
-            arr[b][i]=c++;
-        }
-        b--;
-       }
-        if(l<=r){
-            for(int i=b;i>=t;i--){
-                arr[i][l]=c++;
+        while(l<=r && t<=b){
+            for(int i=l;i<=r;i++){
+                arr[t][i]=c++;
             }
-            l++;
+            t++;
+            for(int i=t;i<=b;i++){
+                arr[i][r]=c++;
+            }
+            r--;
+            if(l<=r){
+                for(int i=r;i>=l;i--){
+                    arr[b][i]=c++;
+                }
+                b--;
+                for(int i=b;i>=t;i--){
+                    arr[i][l]=c++;
+                }
+                l++;
+            }
         }
-       }
         return arr;
     }
 }
