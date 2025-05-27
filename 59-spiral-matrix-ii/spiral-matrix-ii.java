14a48162b140c839e57132515d143ae4cdd6f4b8
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2025-05-27 20:44:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (18.07%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/spiral-matrix-ii.java
@@ -1,12 +1,10 @@
 class Solution {
     public int[][] generateMatrix(int n) {
         int[][] arr=new int[n][n];
-        int t=0;
-        int l=0;
-        int b=n-1, r=n-1;
+        int t=0,b=n-1,l=0,r=n-1;
         int c=1;
-        while(l<=r && t<=b){
-            for(int i=l;i<=r;i++){
+        while(t<=b && l<=r){
+            for(int i=l;i<=b;i++){
                 arr[t][i]=c++;
             }
             t++;
@@ -14,11 +12,13 @@ class Solution {
                 arr[i][r]=c++;
             }
             r--;
-            if(l<=r){
+            if(t<=b){
                 for(int i=r;i>=l;i--){
                     arr[b][i]=c++;
                 }
                 b--;
+            }
+            if(l<=r){
                 for(int i=b;i>=t;i--){
                     arr[i][l]=c++;
                 }
