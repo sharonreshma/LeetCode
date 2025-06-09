c048b8525af65c5e60988115e30677e6b5f26ab4
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2025-06-09 20:54:09 +0530

    Time: 6 ms (98.31%) | Memory: 48 MB (89.71%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -1,23 +1,32 @@
 class Solution {
+    // Helper function to calculate sum of ceilings
+    public int divv(int[] nums, int divisor) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += (num + divisor - 1) / divisor;  // Efficient ceil logic
+        }
+        return sum;
+    }
+
     public int smallestDivisor(int[] nums, int threshold) {
-        int s=1;
-        int l=1000000;
-        while(s<=l){
-            int mid=s+(l-s)/2;
-            if(check(mid,nums)>threshold){
-                s=mid+1;
-            }
-            else{
-                l=mid-1;
+        int s = 1;  // start from 1
+        int e = 0;
+
+        // Find maximum element for upper bound of binary search
+        for (int num : nums) {
+            e = Math.max(e, num);
+        }
+
+        // Binary search
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (divv(nums, mid) <= threshold) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
             }
         }
+
         return s;
     }
-        public int check(int mid,int nums[]){
-            int res=0;
-            for(int i=0;i<nums.length;i++){
-                res+=(nums[i]+mid-1)/mid;
-            }
-          return res;
-        }
 }
