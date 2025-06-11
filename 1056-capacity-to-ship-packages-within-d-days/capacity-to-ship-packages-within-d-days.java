98550698bf65ce7216e105bcc37ad0a0e3d82978
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2025-06-11 23:03:03 +0530

    Time: 11 ms (76.18%) | Memory: 47.2 MB (71.99%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,28 +1,29 @@
 class Solution {
-    public int shipWithinDays(int[] weights, int days) {
-        int maxw=-1,totw=0;
-        for(int weight:weights){
-            maxw=Math.max(maxw,weight);
-            totw+=weight;
+    public int ship(int[] weights,int n){
+        int d=1,load=0;
+        for(int i=0;i<=weights.length-1;i++){
+          if(weights[i]+load>n){
+            d+=1;
+            load=weights[i];
+          }else{
+            load+=weights[i];
+          }
         }
-        int s=maxw,e=totw;
-        while(s<e){
+        return d;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int s=0;
+        int e=0;
+        for(int weight:weights){
+            s=Math.max(s,weight);
+            e+=weight;
+        }
+        while(s<=e){
             int mid=s+(e-s)/2;
-            int sum=0;
-            int c=1;
-            for(int weight:weights){
-                if(sum+weight>mid){
-                    c++;
-                    sum=0;
-                }
-                    sum+=weight;
-                
-
-            }
-            if(c>days){
-                s=mid+1;
+            if(ship(weights,mid)<=days){
+                e=mid-1;
             }else{
-                e=mid;
+                s=mid+1;
             }
         }
         return s;
