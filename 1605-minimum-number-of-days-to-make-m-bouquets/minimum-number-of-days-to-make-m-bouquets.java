82688015951ef41e70e8ae0546b004db846a7bef
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2025-06-11 21:31:21 +0530

    Time: 22 ms (32.60%) | Memory: 55.6 MB (98.26%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,42 +1,41 @@
 class Solution {
     public boolean boq(int[] bloomDay,int mid,int m,int k){
-        int c=0;
-        int b=0;
-        for(int bloomDays:bloomDay){
-         if(bloomDays<=mid){
-              c++;
-         }
-         else{
-            b+=c/k;
-            c=0;
-         }
-        }
-        b+=c/k;
-        if(b>=m){
-            return true;
-        }
-        return false;
+          int nb=0;
+          int c=0;
+          for(int arr:bloomDay){
+            if(arr<=mid){
+                c++;
+            }
+            else{
+                nb+=c/k;
+                c=0;
+            }
+            
+          }
+          nb+=c/k;
+          if(nb>=m){
+                return true;
+            }
+            return false;
     }
     public int minDays(int[] bloomDay, int m, int k) {
+        int s=Integer.MAX_VALUE;
+        int e=Integer.MIN_VALUE;
+        for(int arr:bloomDay){
+           e= Math.max(arr,e);
+            s=Math.min(arr,s);
+        }
+        int ans=-1;
         if(bloomDay.length<m*k){
             return -1;
         }
-        int s=Integer.MAX_VALUE;
-        int l=Integer.MIN_VALUE;
-        
-        for(int bloomDays:bloomDay){
-                l=Math.max(bloomDays,l);
-                s=Math.min(bloomDays,s);
-            
-        }
-        int ans=-1;
-        while(s<=l){
-            int mid=s+(l-s)/2;
+        while(s<=e){
+            int mid=s+(e-s)/2;
             if(boq(bloomDay,mid,m,k)==true){
-                ans=mid;
-                l=mid-1;
+               ans=mid;
+               e=mid-1;
             }else{
-                s=mid+1;
+               s=mid+1;
             }
         }
         return ans;
