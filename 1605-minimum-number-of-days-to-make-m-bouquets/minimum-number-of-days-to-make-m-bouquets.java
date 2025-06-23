b04c6ee9e9c0cd369f50c2db288b1c14f8419493
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2025-06-23 18:48:55 +0530

    Time: 23 ms (20.93%) | Memory: 58.8 MB (20.16%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,41 +1,40 @@
 class Solution {
-    public boolean boq(int[] bloomDay,int mid,int m,int k){
-          int nb=0;
-          int c=0;
-          for(int arr:bloomDay){
-            if(arr<=mid){
+    public boolean boq(int[] bloomDay, int m, int k,int b){
+        int c=0;
+        int nob=0;
+        for(int bb:bloomDay){
+            if(bb<=b){
                 c++;
             }
             else{
-                nb+=c/k;
-                c=0;
+         nob+=c/k;
+         c=0;
             }
-            
-          }
-          nb+=c/k;
-          if(nb>=m){
-                return true;
-            }
-            return false;
+        }
+        nob+=c/k;
+        if(nob>=m){
+            return true;
+        }
+        return false;
     }
     public int minDays(int[] bloomDay, int m, int k) {
         int s=Integer.MAX_VALUE;
         int e=Integer.MIN_VALUE;
-        for(int arr:bloomDay){
-           e= Math.max(arr,e);
-            s=Math.min(arr,s);
-        }
-        int ans=-1;
         if(bloomDay.length<m*k){
             return -1;
         }
+        for(int b:bloomDay){
+            s=Math.min(b,s);
+            e=Math.max(b,e);
+        }
+          int ans=-1;
         while(s<=e){
             int mid=s+(e-s)/2;
-            if(boq(bloomDay,mid,m,k)==true){
-               ans=mid;
-               e=mid-1;
+            if(boq(bloomDay,m,k,mid)==true){
+                ans=mid;
+                e=mid-1;
             }else{
-               s=mid+1;
+                s=mid+1;
             }
         }
         return ans;
