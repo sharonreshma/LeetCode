cad65d8a923d25c7bb91db956155466b015bcbee
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2025-06-23 17:58:49 +0530

    Time: 15 ms (54.42%) | Memory: 45 MB (75.74%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,27 +1,27 @@
 class Solution {
-    public long ban(int[] piles,int h){
-   int tothr=0;
-   for(int pile:piles){
-    tothr+=(double)(pile+h-1)/(double)h;
-   }
-   return tothr;
+    public long ban(int[] piles,int hr){
+        int t=0;
+        for(int pile:piles){
+            t+=(double)(pile+hr-1)/(double)hr;
+        }
+        return t;
     }
     public int minEatingSpeed(int[] piles, int h) {
         int s=1;
-        int l=0;
+        int e=0;
         for(int pile:piles){
-            if(pile>l){
-                l=pile;
+            if(pile>e){
+                e=pile;
             }
         }
-        while(s<=l){
-            int mid=s+(l-s)/2;
+        while(s<=e){
+            int mid=s+(e-s)/2;
             if(ban(piles,mid)<=h){
-                l=mid-1;
+                e=mid-1;
             }else{
                 s=mid+1;
             }
         }
-        return s; 
+        return s;
     }
 }
