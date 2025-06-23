92d2f3f0d8848217ab9cc5c74e3a8800eab23a55
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2025-06-23 17:09:07 +0530

    Time: 0 ms (100.00%) | Memory: 83.5 MB (97.22%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxr(int[][] mat,int c){
-        int m=0;
         int idx=0;
+        int m=0;
         for(int i=0;i<mat.length;i++){
             if(mat[i][c]>m){
                 m=mat[i][c];
-            idx=i;
+                idx=i;
             }
         }
         return idx;
