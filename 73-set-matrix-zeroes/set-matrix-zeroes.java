c5ac45e2be9785b1ec5d788f7c45f49157577918
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2025-06-26 20:46:26 +0530

    Time: 2 ms (17.52%) | Memory: 45.6 MB (63.80%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,37 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public void setZeroes(int[][] matrix) {
-         boolean fr=false,fc=false;
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-                if(matrix[i][j]==0){
-                    if(i==0){
-                    fr=true;
-                    }
-                    if(j==0){
-                        fc=true;
-                    }
-                    matrix[0][j]=0;
-                    matrix[i][0]=0;
-                    }
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> cols = new HashSet<>();
+
+        // Step 1: Find all rows and columns with a 0
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    cols.add(j);
                 }
-        
-        }
-        for (int i = 1; i < matrix.length; i++){
-      for (int j = 1; j < matrix[0].length; j++){
-        if (matrix[i][0] == 0 || matrix[0][j] == 0){
-          matrix[i][j] = 0;}
-      }
-        }
-        if(fr==true){
-            for(int j=0;j<matrix[0].length;j++){
-                matrix[0][j]=0;
-            }
-        }
-        if(fc==true){
-            for(int i=0;i<matrix.length;i++){
-                matrix[i][0]=0;
             }
         }
 
+        // Step 2: Set rows to 0
+        for (int row : rows) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[row][j] = 0;
+            }
+        }
+
+        // Step 3: Set columns to 0
+        for (int col : cols) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][col] = 0;
+            }
+        }
     }
 }
