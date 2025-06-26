804a171aa1e08cc4dd402d54b6ec3fdc767f50d6
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2025-06-26 21:05:08 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (11.58%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,28 +1,4 @@
 class Solution {
-    public void nextPermutation(int[] nums) {
-        int idx=-1;
-        int n=nums.length;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                idx=i;
-                break;
-            }
-        }
-        if(idx==-1){
-            reverse(nums,0,n-1);
-            return;
-        }
-        for(int i=n-1;i>idx;i--){
-            if(nums[i]>nums[idx]){
-                int t=nums[i];
-                nums[i]=nums[idx];
-                nums[idx]=t;
-                break;
-            }
-        }
-
-         reverse(nums,idx+1,n-1);
-    }
     public void reverse(int[] nums,int s,int e){
         while(s<e){
             int t=nums[s];
@@ -31,5 +7,27 @@ class Solution {
             s++;
             e--;
         }
+    }
+    public void nextPermutation(int[] nums) {
+        int idx=-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx=i;
+                break;
+            }
+        }
+        if(idx==-1){
+            reverse(nums,0,nums.length-1);
+            return;
+        }
+        for(int i=nums.length-1;i>=idx;i--){
+            if(nums[i]>nums[idx]){
+                int t=nums[i];
+                nums[i]=nums[idx];
+                nums[idx]=t;
+                 break;
+            }
+        }
+        reverse(nums,idx+1,nums.length-1);
     }
 }
