b7a4ff02ac712e76ac30ea92c1362ebfba81e03d
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2025-06-26 21:28:29 +0530

    Time: 2 ms (17.52%) | Memory: 45.9 MB (27.72%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,33 +1,25 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public void setZeroes(int[][] matrix) {
-        Set<Integer> rows = new HashSet<>();
-        Set<Integer> cols = new HashSet<>();
-
-        // Step 1: Find all rows and columns with a 0
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == 0) {
-                    rows.add(i);
-                    cols.add(j);
+        Set<Integer> r=new HashSet<>();
+        Set<Integer> c=new HashSet<>();
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    r.add(i);
+                    c.add(j);
                 }
             }
         }
-
-        // Step 2: Set rows to 0
-        for (int row : rows) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                matrix[row][j] = 0;
-            }
+        for(int rr:r){
+        for(int j=0;j<matrix[0].length;j++){
+            matrix[rr][j]=0;
+        }
+        }
+        for(int cc:c){
+        for(int i=0;i<=matrix.length-1;i++){
+            matrix[i][cc]=0;
+        }
         }
 
-        // Step 3: Set columns to 0
-        for (int col : cols) {
-            for (int i = 0; i < matrix.length; i++) {
-                matrix[i][col] = 0;
-            }
-        }
     }
 }
