fba007234b7d51d11dc0075ca62bb5a87cb60c5b
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2025-06-27 16:30:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (21.26%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -12,8 +12,8 @@ class Solution {
         int idx=-1;
         for(int i=nums.length-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
-                idx=i;
-                break;
+               idx=i;
+               break;
             }
         }
         if(idx==-1){
@@ -22,12 +22,12 @@ class Solution {
         }
         for(int i=nums.length-1;i>=idx;i--){
             if(nums[i]>nums[idx]){
-                int t=nums[i];
+                int tt=nums[i];
                 nums[i]=nums[idx];
-                nums[idx]=t;
-                 break;
+                nums[idx]=tt;
+                break;
             }
         }
-        reverse(nums,idx+1,nums.length-1);
+reverse(nums,idx+1,nums.length-1);
     }
 }
