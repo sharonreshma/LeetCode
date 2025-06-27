f196ea1e103452e5f52464ba52b374fe0a496371
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2025-06-27 18:13:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (27.12%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -1,31 +1,34 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         ArrayList<Integer> list=new ArrayList<>();
-        int m=matrix.length;
-        int n=matrix[0].length;
-        int t=0,b=m-1,l=0,r=n-1;
+        int t=0;
+        int b=matrix.length-1;
+        int l=0;
+        int r=matrix[0].length-1;
         while(t<=b && l<=r){
-            for(int i=l;i<=r;i++){
+        for(int i=l;i<=r;i++){
             list.add(matrix[t][i]);
-            }
-            t++;
-            for(int i=t;i<=b;i++){
-            list.add(matrix[i][r]);
-            }
-            r--;
-            if(t<=b){
-                for(int i=r;i>=l;i--){
-                    list.add(matrix[b][i]);
-                }
-                b--;
-            }
-            if(l<=r){
-                for(int i=b;i>=t;i--){
-                    list.add(matrix[i][l]);
-                }
-                l++;
-            }
         }
+        t++;
+        for(int i=t;i<=b;i++){
+            list.add(matrix[i][r]);
+        }
+        r--;
+        if(t<=b){
+            for(int i=r;i>=l;i--){
+            list.add(matrix[b][i]);
+             }
+             b--;
+        }
+        if(l<=r){
+             for(int i=b;i>=t;i--){
+            list.add(matrix[i][l]);
+             }
+             l++;
+        }
+        
+        }
+        
         return list;
     }
 }
