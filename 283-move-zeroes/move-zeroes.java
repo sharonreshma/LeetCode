be9d60a19bc1e2a7e973f5a2e5550ec971c488fd
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2025-06-30 20:49:22 +0530

    Time: 1 ms (99.75%) | Memory: 46.2 MB (29.96%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,11 +1,8 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int c=0;
         int k=0;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                c++;
-            }else{
+            if(nums[i]!=0){
                 nums[k]=nums[i];
                 k++;
             }
@@ -13,5 +10,6 @@ class Solution {
         for(int i=k;i<nums.length;i++){
             nums[i]=0;
         }
+    
     }
 }
