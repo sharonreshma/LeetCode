df9f3f833a0e312d436fc8f18ffe7c1bcd844a27
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2025-07-02 23:26:12 +0530

    Time: 5 ms (53.51%) | Memory: 44.6 MB (91.95%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -2,22 +2,21 @@ class Solution {
     public int romanToInt(String s) {
         HashMap<Character,Integer> h=new HashMap<>();
         h.put('I',1);
-        h.put('V', 5);
-        h.put('X', 10);
-        h.put('L', 50);
-        h.put('C', 100);
-        h.put('D', 500);
-        h.put('M', 1000);
-    int tot=0,p=0;
-    for(int i=s.length()-1;i>=0;i--){
-        int c=h.get(s.charAt(i));
-        if(c<p){
-            tot-=c;
-        }else{
-            tot+=c;
+        h.put('V',5);
+        h.put('X',10);
+        h.put('L',50);
+        h.put('C',100);
+        h.put('D',500);
+        h.put('M',1000);
+        int t=0,p=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(h.get(s.charAt(i))<p){
+               t-=h.get(s.charAt(i));
+            }else{
+                t+=h.get(s.charAt(i));
+            }
+            p=h.get(s.charAt(i));
         }
-        p=c;
-    }
-    return tot;
+        return t;
     }
 }
