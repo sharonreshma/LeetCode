c18a6895c0ab6c1c6ab2561bbdf9de9013081540
Sharon Reshma A <125342483+sharonreshma@users.noreply.github.com>
2025-07-04 20:09:39 +0530

    Time: 6 ms (34.10%) | Memory: 41.6 MB (93.48%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,25 +1,32 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        int c=0,m=0;
+        Set<Character> h=new HashSet<>();
+        int c=0;
+        int d=0;
+        h.add('a');
+        h.add('e');
+        h.add('i');
+        h.add('o');
+        h.add('u');
+        h.add('A');
+        h.add('E');
+        h.add('I');
+        h.add('O');
+        h.add('U');
         for(int i=0;i<s.length()/2;i++){
-        if((s.charAt(i)=='a')|| (s.charAt(i)=='e')||(s.charAt(i)=='i')||
-        (s.charAt(i)=='o')||(s.charAt(i)=='u'||s.charAt(i)=='A')|| 
-        (s.charAt(i)=='E')||(s.charAt(i)=='I')||
-        (s.charAt(i)=='O')||(s.charAt(i)=='U')){
-       c++;
-        }
+            if(h.contains(s.charAt(i))){
+                c++;
+            }
         }
         for(int i=s.length()/2;i<s.length();i++){
-if((s.charAt(i)=='a')|| (s.charAt(i)=='e')||(s.charAt(i)=='i')||
-        (s.charAt(i)=='o')||(s.charAt(i)=='u'||s.charAt(i)=='A')|| 
-        (s.charAt(i)=='E')||(s.charAt(i)=='I')||
-        (s.charAt(i)=='O')||(s.charAt(i)=='U')){
-       c--;
+            if(h.contains(s.charAt(i))){
+                d++;
+            }
         }
-        }
-        if(c==0){
+        if(c==d){
             return true;
         }
         return false;
+
     }
 }
